bf3c72f79032b2321a8204975363a752e2e96234
nicolas.boisseau <nicolas.boisseau@outlook.com>
2024-12-06 09:09:08 +0100

    day 6 part 2 ok (but slow :D)

--- day6/impl.py
+++ day6/impl.py
@@ -4,11 +4,11 @@ from common.common import download_input_if_not_exists, post_answer, capture, ca
 
 download_input_if_not_exists(2024)
 
-def found_guard(lines):
+def find_guard(lines):
     for y in range(len(lines)):
         for x in range(len(lines[y])):
             if lines[y][x] == "^":
-                print(f"Found ^ at {x}, {y}")
+                #print(f"Found ^ at {x}, {y}")
                 return (y, x), (-1, 0)
 
 def print_map(lines):
@@ -32,39 +32,86 @@ def move_guard(guard, dir, lines):
     if y + dy < 0 or y + dy >= len(lines) or x + dx < 0 or x + dx >= len(lines[y]):
         raise ValueError("Out of bounds")
 
-    if lines[y + dy][x + dx] == "#":
+    if lines[y + dy][x + dx] == "#" or lines[y + dy][x + dx] == "O":
         return move_guard(guard, turn_dir_right(dir), lines)
     else:
+        if lines[y + dy][x + dx] in ["^", "v", "<", ">"] and get_symbol_by_dir(dir) == lines[y + dy][x + dx]:
+            raise Exception("Loop detected")
         return (y + dy, x + dx), dir
 
 def score(lines):
     score = 0
     for y in range(len(lines)):
         for x in range(len(lines[y])):
-            if lines[y][x] == "X" or lines[y][x] == "^":
+            if lines[y][x] == "X" or lines[y][x] == "^" or lines[y][x] == "v" or lines[y][x] == "<" or lines[y][x] == ">":
                 score += 1
     return score
 
-def part1(lines):
-    guard, dir = found_guard(lines)
-
-    #print_map(lines)
+def compute_score_and_output(lines):
+    guard, dir = find_guard(lines)
 
     while True:
         try:
             guard, dir = move_guard(guard, dir, lines)
             y, x = guard
-            lines[y] = lines[y][:x] + "X" + lines[y][x+1:]
-            #print(score(lines))
-            #print_map(lines)
+            symbol = get_symbol_by_dir(dir)
+            lines[y] = lines[y][:x] + symbol + lines[y][x + 1:]
+            # print(score(lines))
+            # print_map(lines)
         except ValueError:
-            print("Out of bounds")
+            #print("Out of bounds")
             break
 
-    return score(lines)
+    return score(lines), lines
+
+def part1(lines):
+    final_score, _ = compute_score_and_output(lines)
+
+    return final_score
+
+
+def get_symbol_by_dir(direction):
+    symbol = "X"
+    if direction == (-1, 0):
+        symbol = "^"
+    elif direction == (0, 1):
+        symbol = ">"
+    elif direction == (1, 0):
+        symbol = "v"
+    elif direction == (0, -1):
+        symbol = "<"
+    return symbol
+
 
 def part2(lines):
-    return 4
+    saved_map = lines.copy()
+    _, final_map = compute_score_and_output(lines)
+
+    # compute all new maps with placing a symbol "O" in the way of the guard
+    all_maps = []
+    for y in range(len(final_map)):
+        for x in range(len(final_map[y])):
+            if final_map[y][x] in ["X", "^", "v", "<", ">"]:
+                new_map = saved_map.copy()
+                new_map[y] = new_map[y][:x] + "O" + new_map[y][x + 1:]
+                #print_map(new_map)
+                #print("")
+                all_maps.append(new_map)
+
+    # now try each map and detect if the guard is stuck in a loop
+    nb_loops = 0
+    for m in all_maps:
+        try:
+            _, _ = compute_score_and_output(m)
+        except Exception as e:
+            if str(e) == "Loop detected":
+                nb_loops += 1
+                continue
+        except ValueError:
+            continue
+
+    return nb_loops
+
 
 
 if __name__ == '__main__':
--- day6/tests.py
+++ day6/tests.py
@@ -14,10 +14,10 @@ class AdventOfCodeTests(unittest.TestCase):
         self.assertEqual(5153, part1(read_input_lines("input.txt")))
 
     def test_part2_sample(self):
-        self.assertEqual(2, part2(read_input_lines("sample.txt")))
+        self.assertEqual(6, part2(read_input_lines("sample.txt")))
 
     def test_part2_input(self):
-        self.assertEqual(4, part2(read_input_lines("input.txt")))
+        self.assertEqual(1711, part2(read_input_lines("input.txt")))
 
 
 if __name__ == '__main__':
