131be858285d937eba10f360993664b757a2f1ca
nicolas.boisseau <nicolas.boisseau@cdiscount.com>
2024-12-16 17:40:03 +0100

    day 16 wip

--- day16/custom_astar.py
+++ day16/custom_astar.py
@@ -27,21 +27,21 @@ class CustomAStar(AStar):
             if current == goal:
                 return 0
             if goal in self.nodes[current]:
-                return 1
+                return -1
 
             if self.part == 1:
               # use a sub A* to find the shortest path between current and goal
-              sub_astar = BasicAStar(self.nodes)
-              return len(list(sub_astar.astar(current, goal)))
+              sub_astar = CustomAStar(self.nodes, use_adminissible_heuristic=True, part=2)
+              return -len(list(sub_astar.astar(current, goal)))
             else:
                 # part 2 : use euclidian distance
-                x1, y1 = map(int, current.split(","))
-                x2, y2 = map(int, goal.split(","))
+                x1, y1 = map(int, current.split(",")[0:2])
+                x2, y2 = map(int, goal.split(",")[0:2])
                 return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
 
 
-        return 1
+        return -1
 
     def is_goal_reached(self, current, goal):
         return current == goal
