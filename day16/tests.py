88026e5ac6411578af8f2a71c7d8faed43cc5e5c
nicolas.boisseau <nicolas.boisseau@outlook.com>
2024-12-16 18:19:32 +0100

    day 16 part 1 OK

--- day16/tests.py
+++ day16/tests.py
@@ -7,10 +7,12 @@ from impl import part1, part2
 class AdventOfCodeTests(unittest.TestCase):
 
     def test_part1_sample(self):
-        self.assertEqual(0, part1(read_input_lines("sample.txt")))
+        self.assertEqual(7036, part1(read_input_lines("sample.txt")))
 
     def test_part1_input(self):
-        self.assertEqual(1, part1(read_input_lines("input.txt")))
+        # 123492 is too high
+        # 123492 is too high
+        self.assertEqual(122492, part1(read_input_lines("input.txt")))
 
     def test_part2_sample(self):
         self.assertEqual(2, part2(read_input_lines("sample.txt")))
