04f9447e5a24c8b5bd399dc286c514f55cf41ada
nicolas.boisseau <nicolas.boisseau@cdiscount.com>
2024-12-26 16:26:48 +0100

    wip day 24 part 2

--- day24/impl.py
+++ day24/impl.py
@@ -22,11 +22,23 @@ def extract_values_and_wires(lines):
 def part1(lines):
     values, wires = extract_values_and_wires(lines)
 
+    run_machine(values, wires)
+
+    print("END !")
+
+    z_keys = [k for k in values.keys() if k[0] == "z"]
+    z_keys.sort(reverse=True)
+    binary = "".join([str(values[k]) for k in z_keys])
+    print("z values : ", z_keys)
+    print("z binary = ", binary)
+
+    return int(binary, 2)
+
+
+def run_machine(values, wires):
     values_to_wait = [w[3] for w in wires if w[3][0] == "z"]
-
-    #print(values)
-    #print(wires)
-
+    # print(values)
+    # print(wires)
     while not all([v_wait in values.keys() for v_wait in values_to_wait]):
         for w in wires:
             if w[0] in values and w[2] in values.keys():
@@ -37,20 +49,35 @@ def part1(lines):
                 elif w[1] == "XOR":
                     values[w[3]] = values[w[0]] ^ values[w[2]]
 
-    print("END !")
-
-    z_keys = [k for k in values.keys() if k[0] == "z"]
-
-    z_keys.sort(reverse=True)
-
-    binary = "".join([str(values[k]) for k in z_keys])
-
-    print("binary result = ", binary)
-
-    return int(binary, 2)
 
 def part2(lines):
-    return part1(lines)
+    values, wires = extract_values_and_wires(lines)
+
+    x_keys = [k for k in values.keys() if k[0] == "x"]
+    x_keys.sort(reverse=True)
+
+    binary = "".join([str(values[k]) for k in x_keys])
+    print("x values : ", x_keys)
+    print("x binary =  ", binary)
+
+    y_keys = [k for k in values.keys() if k[0] == "y"]
+    y_keys.sort(reverse=True)
+
+    binary = "".join([str(values[k]) for k in y_keys])
+    print("y values : ", y_keys)
+    print("y binary =  ", binary)
+
+    # ACT
+    run_machine(values, wires)
+
+    z_keys = [k for k in values.keys() if k[0] == "z"]
+    z_keys.sort(reverse=True)
+    binary = "".join([str(values[k]) for k in z_keys])
+    print("z values : ", z_keys)
+    print("z binary = ", binary)
+
+    return 0
+
 
 
 if __name__ == '__main__':
